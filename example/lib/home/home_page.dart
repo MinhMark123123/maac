6bd0beac08c3006666463eda8c433656298dd740
MinhMark2287 <ngocminh29011991@gmail.com>
2023-02-12 03:16:17 +0700

    Feature/check stucture (#2)
    * update new structure
    
    ---------
    
    Co-authored-by: Minh Nguyen <minh.nguyenngoc4@novatechgroup.vn>
    Co-authored-by: YenMinh <minh.nguyen.yen72@gmail.com>
    Co-authored-by: MinhNguyen <minhnguyen@MinhNguyens-MacBook-Pro.local>

---
+++ example/lib/home/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:example/global.dart';
+import 'package:example/navigation/routers.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('Hi there:'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ref.read(navigationProvider).go(AppRoutes.home + AppRoutes.second);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
