3f5508bfde87ca8549bd33d524e8ae31c9a5ca30
MinhMark2287 <ngocminh29011991@gmail.com>
2023-02-12 03:16:17 +0700

    Feature/check stucture (#2)
    * update new structure
    
    ---------
    
    Co-authored-by: Minh Nguyen <minh.nguyenngoc4@novatechgroup.vn>
    Co-authored-by: YenMinh <minh.nguyen.yen72@gmail.com>
    Co-authored-by: MinhNguyen <minhnguyen@MinhNguyens-MacBook-Pro.local>

---
+++ maac_core/lib/src/ui/stream_data_consumer.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/widgets.dart';
+import 'package:maac_core/maac_core.dart';
+
+class StreamDataConsumer<Data> extends StatelessWidget {
+  final StreamData<Data> _streamData;
+  final Widget Function(BuildContext context, Data data) _builder;
+
+  const StreamDataConsumer({
+    Key? key,
+    required StreamData<Data> streamData,
+    required Widget Function(BuildContext context, Data data) builder,
+  })  : _streamData = streamData,
+        _builder = builder,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<Data>(
+      initialData: _streamData.data,
+      stream: _streamData.asStream(),
+      builder: (context, snapshot) {
+        return _builder(context, _streamData.data);
+      },
+    );
+  }
+}
