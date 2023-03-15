e7d256acfaabdd1a533244788af2dc353a1ba696
MinhNguyen <minhnguyen@MinhNguyens-MacBook-Pro.local>
2023-03-16 01:50:00 +0700

    support onPause and onResume

--- maac_core/lib/src/ui/app_view_model_widget.dart
+++ maac_core/lib/src/ui/app_view_model_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:maac_core/src/ui/app_life_state.dart';
 import 'package:maac_core/src/view_model/view_model.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 abstract class ViewModelWidget<T extends ViewModel> extends ViewStatefulWidget {
   const ViewModelWidget({Key? key}) : super(key: key);
@@ -17,10 +18,16 @@ abstract class ViewModelWidget<T extends ViewModel> extends ViewStatefulWidget {
 }
 
 class _BindViewModelWidgetState extends ViewState<ViewModelWidget> {
+  final _visibilityDetectorKey = UniqueKey();
 
   @override
   Widget build(BuildContext context) {
-    return widget.build(context, viewModels.first);
+    print("${widget.runtimeType} build");
+    return VisibilityDetector(
+      onVisibilityChanged: _onVisibilityChanged,
+      key: _visibilityDetectorKey,
+      child: widget.build(context, viewModels.first),
+    );
   }
 
   @override
@@ -30,5 +37,19 @@ class _BindViewModelWidgetState extends ViewState<ViewModelWidget> {
   void aWake() {
     super.aWake();
     widget.aWake(viewModels.first);
+  }
+
+  @override
+  void deactivate() {
+    for (var element in viewModels) {
+      element.onPause();
+    }
+    super.deactivate();
+  }
+
+  void _onVisibilityChanged(VisibilityInfo info) {
+    for (var element in viewModels) {
+      element.onVisibilityChanged(info);
+    }
   }
 }
