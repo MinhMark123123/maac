633245e67e8cdb9157b5c959c30f11e6391a35fc
MinhMark123123 <minhnn@guide.inc>
2025-07-25 16:50:20 +0700

    fix: fix get view model

--- packages/maac_mvvm_with_get_it/lib/src/get.dart
+++ packages/maac_mvvm_with_get_it/lib/src/get.dart
@@ -2,10 +2,13 @@ import 'package:get_it/get_it.dart';
 import 'package:maac_mvvm/maac_mvvm.dart';
 
 final _sl = GetIt.asNewInstance();
+
+/// create viewmodel instance from [registerViewModel] by _sl.get<T extends ViewModel>
 var injectViewModel = _sl;
 
 Future<void> resetViewModelSL() => _sl.reset();
 
+/// register viewmodel instance to [GetIt] with [_sl] custom instance
 void registerViewModel<T extends ViewModel>(
   T Function() factoryFunc, {
   String? instanceName,
@@ -19,7 +22,7 @@ T getViewModel<T extends ViewModel>({
   String? instanceName,
   Type? type,
 }) {
-  return _sl.get<T>(
+  return GetIt.instance.get<T>(
     param1: param1,
     param2: param2,
     instanceName: instanceName,
